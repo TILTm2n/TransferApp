11167c2c911320363d3716c9780766ebc41acf4d
Eugene <evgen29031999@bk.ru>
2022-04-04 21:55:16 +0300

    passing data fron A to B

---
+++ TransferApp/SecondViewController.swift
@@ -0,0 +1,43 @@
+//
+//  SecondViewController.swift
+//  TransferApp
+//
+//  Created by Eugene on 04.04.2022.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    var updatingData: String = ""
+    
+    @IBOutlet weak var dataTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        updateTextFieldData(withText: updatingData)
+        
+    }
+    
+    private func updateTextFieldData(withText text: String) {
+        dataTextField.text = text
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- TransferApp/ViewController.swift
+++ TransferApp/ViewController.swift
@@ -9,11 +9,32 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var updatedData = "Test Data"
+    
+    @IBOutlet weak var dataLabel: UILabel!
+    
+    @IBAction func editDataWithProperty(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let editScreen = storyboard.instantiateViewController(withIdentifier: "SecondViewController") as! SecondViewController
+        
+        editScreen.updatingData = dataLabel.text ?? ""
+        
+        self.navigationController?.pushViewController(editScreen, animated: true)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
 
-
+    private func updateLabel(withText text: String) {
+        dataLabel.text = updatedData
+    }
+    
 }
 
